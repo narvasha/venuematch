ed31ee69833c09c44134ca90bf14f02349d447b6
Soul-Xettri <rakeshchhetri420@gmail.com>
2023-09-16 14:45:35 +0545

    url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/venuematch/",
   build: {
     outDir: 'docs', // You can change 'docs' to any directory name you prefer
   },
